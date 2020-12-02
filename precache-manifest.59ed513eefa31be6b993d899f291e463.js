0db7a0d736a568d2ee4b90f3620c60a09ad3ef8a
CaioDePaula <caio.teodoro@ftdata.com.br>
2020-12-01 23:46:22 -0300

    Updates

---
+++ precache-manifest.59ed513eefa31be6b993d899f291e463.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec0b0c5e29b6c0eecdc3685cba9d7c4",
+    "url": "/myhouse-web/index.html"
+  },
+  {
+    "revision": "7cf999e58d4a5e2b0d7a",
+    "url": "/myhouse-web/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "cce101232cad34fd37ae",
+    "url": "/myhouse-web/static/css/main.cf3066cd.chunk.css"
+  },
+  {
+    "revision": "7cf999e58d4a5e2b0d7a",
+    "url": "/myhouse-web/static/js/2.dec26cd1.chunk.js"
+  },
+  {
+    "revision": "9fae22ddfdc3536517d9493db28374be",
+    "url": "/myhouse-web/static/js/2.dec26cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce101232cad34fd37ae",
+    "url": "/myhouse-web/static/js/main.18c35d4d.chunk.js"
+  },
+  {
+    "revision": "2909aff104d9e826e0b0",
+    "url": "/myhouse-web/static/js/runtime-main.3b6d2e60.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/myhouse-web/static/media/ilustration.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/myhouse-web/static/media/logo.1653c647.svg"
+  }
+]);
